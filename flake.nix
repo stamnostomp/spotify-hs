6ab8faff61cb1c26553f905083a0d8e2be395cde
stamno <stamno@pm.me>
2026-01-18 19:39:47 -0700

    switch to flake utils

--- flake.nix
+++ flake.nix
@@ -3,25 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          ghc
-          cabal2nix
-        ];
-
-        shellHook = ''
-          echo "spotify.hs dev shell"
-        '';
-      };
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ghc
+            cabal2nix
+          ];
+        };
+      }
+    );
 }
